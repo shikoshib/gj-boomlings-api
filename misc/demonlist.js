61ab4732c598139eafda22e7d66d6b6c0673a5ed
Matvey <shikoshib@yandex.ru>
2023-07-25 09:40:59 +1000

    Add files via upload

--- misc/demonlist.js
+++ misc/demonlist.js
@@ -1,16 +1,16 @@
 module.exports = {
     demonlist:
-        async function(name) {
+        async function (name) {
             let r = await fetch(`https://pointercrate.com/api/v2/demons/?name=${name}`);
             let lvlObj = await r.json();
-            if(lvlObj == undefined) return null;
-            
+            if (!lvlObj.length) return null;
+
             const res = {
                 "position": lvlObj[0].position,
                 "publisher": lvlObj[0].publisher.name,
                 "verifier": lvlObj[0].verifier.name,
             }
-                
+
             return res;
         }
 }
