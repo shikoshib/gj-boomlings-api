18888986317202894a6008f4ce981f08f2d1fcc6
Matvey <shikoshib@yandex.ru>
2023-07-24 16:41:51 +1000

    Delete functions directory

--- functions/getDailyLevel.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    getDailyLevel:
-        async function() {
-            const { dlLevel } = require("./dlLevel.js")
-            let res = await dlLevel(-1);
-            return res;
-        }
-}
--- functions/getWeeklyDemon.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    getWeeklyDemon:
-        async function() {
-            const { dlLevel } = require("./dlLevel.js")
-            let res = await dlLevel(-2);
-            return res;
-        }
-}
